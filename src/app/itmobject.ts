28096bf47f18742c1e0e5b216247ac826731b9ed
Patrick Eradus <p.eradus@64kb.nl>
2019-01-30 13:08:41 +0100

    update

--- src/app/itmobject.ts
+++ src/app/itmobject.ts
@@ -1,6 +1,7 @@
 export interface IITMObjectProperty {
   displayName: string;
-  description: string;
+  hint?: string;
+  description?: string;
   validator?: string;
   value: string;
 }
@@ -21,8 +22,8 @@ export interface IITMObjectMethods {
 export interface IITMObjectData {
   name: string;
   className: string;
-  shortDescription: string;
-  description: string;
+  shortDescription?: string;
+  description?: string;
   methods?: IITMObjectMethods;
   properties?: IITMObjectProperties;
  }
@@ -37,14 +38,5 @@ export interface IITMObjectData {
 
 export class Itmobject {
   constructor (public data: IITMObjectData) {
-  //  this.name = 'anITMObject';
-  //  this.className = 'ITMObjectClass';
-  //  this.shortDescription = 'This is an short description';
-  //  this.description = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean volutpat bibendum egestas.
-  //  Vestibulum imperdiet consectetur nibh, bibendum tristique tellus ornare et. Cras at mi vel sem rutrum placerat.
-  //  Suspendisse maximus tincidunt scelerisque. Sed eget placerat magna. Fusce egestas, felis vitae pharetra tempor,
-  //  enim diam finibus diam, nec euismod nunc neque eu leo. Duis bibendum facilisis rhoncus.
-  //  Vivamus nulla ipsum, feugiat quis sollicitudin id, mollis eu nisl. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-  //  Vestibulum accumsan quam at rhoncus finibus. Quisque purus velit, tincidunt in risus aliquet, eleifend venenatis arcu. `;
   }
 }
