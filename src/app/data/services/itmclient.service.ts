6648a979bad3cbc44a3b3ebf167b8872e56595bc
Patrick Eradus <p.eradus@64kb.nl>
2019-12-23 15:46:13 +0100

    code review

--- src/app/data/services/itmclient.service.ts
+++ src/app/data/services/itmclient.service.ts
@@ -10,55 +10,6 @@ export class ItmClientService {
 
   itmObject: IITMObject;
   constructor(private http: HttpClient)  {
-
-    this.itmObject = {
-      name: 'anITMObject',
-        className: 'ITMObjectClass',
-        displayName: 'a short description',
-        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam mollis nec
-        nisi ac vestibulum. Phasellus facilisis elit enim, eget convallis dui gravida quis. Pellentesque eu
-          eros eget ligula hendrerit consectetur nec vitae sapien. Vivamus aliquam nec lorem in scelerisque.
-          Suspendisse blandit luctus augue, a faucibus lectus tincidunt id. Aenean porttitor neque non
-            pulvinar consectetur. Integer et elit non mauris blandit semper ut vel eros.`,
-        methods: [
-          {
-            name: 'stop'
-          },
-          {
-            name: 'start'
-          }
-        ],
-        properties: [
-          {
-            'name' : 'testproperty',
-            'description' : 'this is a test property',
-            'value' : '123'
-          },
-          {
-            'name' : 'testproperty2',
-            'description' : 'this is a test property2',
-            'value' : '123'
-          },
-          {
-            'name' : 'testproperty3',
-            'description' : 'this is a test property3',
-            'value' : '123'
-          },
-          {
-            'name' : 'testproperty4',
-            'description' : 'this is a test property4',
-            'value' : '123'
-          }
-        ],
-        instances: [
-          {
-            name: 'instance1'
-          },
-          {
-            name: 'instance2'
-          }
-        ]
-      };
   }
 
   getItmObject(instance: string) {
